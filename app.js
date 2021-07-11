c0ab4314c7a61671b70334ae9094e05fe4e511ce
aravindhsharma <49915185+aravindhsharma@users.noreply.github.com>
2021-07-11 08:44:19 +0530

    Add .gitignore and update ports

--- app.js
+++ app.js
@@ -6,87 +6,91 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 const PORT = process.env.PORT || 3000;
 
-const homeStartingContent = "Welcome to the Daily Journal blog site, where you can find some of the interesting articles regarding the Web Development.";
-const aboutContent = "This is a simple blog site made with HTML, CSS, JavaScript, EJS, Nodejs and connected to the MongoDB server.";
-const contactContent = "If you want to see more interesting articles regarding web development, give a shout-out to me at dailyjournal@email.com";
+const homeStartingContent =
+  "Welcome to the Daily Journal blog site, where you can find some of the interesting articles regarding the Web Development.";
+const aboutContent =
+  "This is a simple blog site made with HTML, CSS, JavaScript, EJS, Nodejs and connected to the MongoDB server.";
+const contactContent =
+  "If you want to see more interesting articles regarding web development, give a shout-out to me at dailyjournal@email.com";
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://aravindhsharma:Srinidhi2002@cluster0.cput3.mongodb.net/blogDB?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  "mongodb+srv://aravindhsharma:Srinidhi2002@cluster0.cput3.mongodb.net/blogDB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const postSchema = {
   title: String,
-  content: String
+  content: String,
 };
 
 const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function (req, res) {
-
   Post.find({}, function (err, posts) {
     res.render("home", {
       startingContent: homeStartingContent,
-      posts: posts
+      posts: posts,
     });
   });
-
 });
-
 
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
 app.post("/compose", function (req, res) {
-
   const post = new Post({
     title: req.body.postTitle,
-    content: req.body.postBody
+    content: req.body.postBody,
   });
 
   post.save(function (err) {
     if (!err) {
       res.redirect("/");
-
     }
-
   });
-
 });
 
 app.get("/posts/:postId", function (req, res) {
   const requestedPostId = req.params.postId;
 
-  Post.findOne({
-    _id: requestedPostId
-  }, function (err, post) {
-    res.render("post", {
-      title: post.title,
-      content: post.content
-    });
-  });
+  Post.findOne(
+    {
+      _id: requestedPostId,
+    },
+    function (err, post) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  );
 });
 
 app.get("/about", function (req, res) {
   res.render("about", {
-    aboutContent: aboutContent
+    aboutContent: aboutContent,
   });
 });
 
 app.get("/contact", function (req, res) {
   res.render("contact", {
-    contactContent: contactContent
+    contactContent: contactContent,
   });
 });
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log("Listening on PORT " + 3000));
